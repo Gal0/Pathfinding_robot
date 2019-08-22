29eca0b4b249efc416bccdf1bc8d342427258519
Robin Drescher <53396528+Gal0@users.noreply.github.com>
2019-08-23 01:02:01 +0200

    Update MotorController.py

--- MotorController.py
+++ MotorController.py
@@ -1,5 +1,5 @@
 #Date: 07.08.2019
-#Author: Robin Drescher
+#Author: Robin Drescher with help from https://tutorials-raspberrypi.de/gpios-mittels-i2c-port-expander-erweitern/ for the implementation of the port expander.
 
 import smbus
 import time
